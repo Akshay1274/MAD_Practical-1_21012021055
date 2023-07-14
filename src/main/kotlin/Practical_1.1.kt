1c1596fe5e99114b89beefb5b4be2df8c08e94a3
student <student@gnu.ac.in>
2023-07-14 12:33:07 +0530

    this is practical 1

---
+++ src/main/kotlin/Practical_1.1.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var a= 89;
+    println("Integer value :$a")
+    var b= 1.9;
+    println("Float value :$b")
+    var c : Char = 'T';
+    println("Character Value :$c")
+    var d = 67.89;
+    println("Double value :$d")
+    var e = 6;
+    println("Short value :$e")
+    var f =127 ;
+    println("byte Value :$f")
+    var g = 3485763;
+    println("Long value : $g")
+    var h : Boolean = true;
+    println("Boolean value : $h")
+    var s : String = "Akshay"
+    println("String value : $s")
+
+}
