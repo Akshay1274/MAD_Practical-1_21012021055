51cccf2f7621151f26256c788a267c2e3520864c
student <student@gnu.ac.in>
2023-07-19 08:59:46 +0530

    practical 1.4 ,1.5

---
+++ src/main/kotlin/Practical_1.5.kt
@@ -0,0 +1,20 @@
+fun main() {
+   print("Enter month Number:")
+    val month = readLine()!!.toString();
+    when(month){
+        1.toString() -> print("January")
+        2.toString()-> print("February")
+        3.toString()-> print("March")
+        4.toString()-> print("April")
+        5.toString()-> print("May")
+        6.toString()-> print("June")
+        7.toString()-> print("July")
+        8.toString()-> print("August")
+        9.toString()-> print("September")
+        10.toString()-> print("october")
+        11.toString()-> print("November")
+        12.toString()-> print("December")
+        13.toString()-> print("Enter VAlid Number!")
+
+    }
+}
